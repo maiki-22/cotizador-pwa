85b4d80147cdd6204841efa3474f44de71789c52
maiki-22 <miguel.cortes.sandoval23@gmail.com>
2025-10-09 17:10:14 -0300

    C:/Program Files/Git/ jeje

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
         background_color: "#ffffff",
         theme_color: "#0ea5e9",
         icons: [
-          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icons/icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icons/icon-512.png", sizes: "512x512", type: "image/png" },
         ],
       },
       workbox: {
